f3579352f30c7457eda49045154d40f0fcc8c7c0
jfiala <johannes.fiala@fwd.at>
2015-11-06 21:19:57 +0100

    Add allowed Fieldnames-list to FB-Request

--- user-rest-service-2.2.2-facebook/src/main/java/at/fwd/swagger/spring/demo/user/controller/connectors/FacebookController.java
+++ user-rest-service-2.2.2-facebook/src/main/java/at/fwd/swagger/spring/demo/user/controller/connectors/FacebookController.java
@@ -5,11 +5,16 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import at.fwd.swagger.spring.demo.user.model.facebook.FacebookUser;
@@ -37,7 +42,7 @@ public class FacebookController {
     @ApiOperation(value="search for users by name-part", notes="search for users")
     @ApiResponses(value = { 
     	    @ApiResponse(code = 200, message = SUCCESS, response = FacebookUser.class) })
-    public FacebookUser findByName(@RequestParam(value="name", required=true) String name, @RequestParam(required=true) String access_token, @RequestParam(required=true)  String fieldlist) throws NoSuchFieldException, SecurityException, NoSuchMethodException {
+    public FacebookUser findByName(@RequestParam(value="name", required=true) String name, @RequestParam(required=true) String access_token, @RequestParam (required=false) String fieldlist) throws NoSuchFieldException, SecurityException, NoSuchMethodException, Exception {
 		
 		RestTemplate restTemplate = new RestTemplate();
 		
@@ -46,19 +51,62 @@ public class FacebookController {
 		// new: https://graph.facebook.com/v2.5/pivotalsoftware?access_token=CAACEdEose0cBAKhO7dPXDpZCELYncZAlZCOSWElLkhooVrP7dNYZCjg6CKS1RibzLSe9M0zfl9WKAWqGcVZAxUdaCyvrv0rujFgtM6YqbVuZANWi2Q8yWOA03CpTeB3FXiADrXDMfRMbZCJtZC7fm72S6k7xyNoxYqvSzjCE0D2Tl705DJFcHZB9DDbfZC6fhwrz5iaWvhZC3uCbf3zXSWtDedX&fields=name,about&metadata=1
 		
 		// dynamically read out a property via annotation
-    	//JsonProperty jsonProperty = FacebookUser.class.getMethod("getBestPage").getAnnotation(JsonProperty.class);
+    	//
 		//System.out.println("Annotation for best page: " + jsonProperty.value());
     	
     	String apiUrl = "https://graph.facebook.com/v2.5/";
     	//String access_token = "CAACEdEose0cBAKhO7dPXDpZCELYncZAlZCOSWElLkhooVrP7dNYZCjg6CKS1RibzLSe9M0zfl9WKAWqGcVZAxUdaCyvrv0rujFgtM6YqbVuZANWi2Q8yWOA03CpTeB3FXiADrXDMfRMbZCJtZC7fm72S6k7xyNoxYqvSzjCE0D2Tl705DJFcHZB9DDbfZC6fhwrz5iaWvhZC3uCbf3zXSWtDedX";
     	//String fieldlist = "&fields=name,about,mission";
     	
-    	String url = apiUrl + name + "?access_token=" + access_token + "&fieldlist=" + fieldlist;
+    	List<String> allowedFieldnames = new ArrayList<String>();
+    	allowedFieldnames.add("access_token");
+    	allowedFieldnames.add("id");
+    	allowedFieldnames.add("name");
+    	allowedFieldnames.add("about");
+    	allowedFieldnames.add("mission");
+    	allowedFieldnames.add("description");
+    	allowedFieldnames.add("app_id");
+    	allowedFieldnames.add("attire");
+    	//allowedFieldnames.add("location");
+    	
+    	
+    	
+    	StringBuffer buffer = new StringBuffer();
+    	
+    	Method[] methods = FacebookUser.class.getMethods();
+    	for (Method method : methods) {
+    		JsonProperty jsonProperty =  method.getAnnotation(JsonProperty.class);
+    		
+    		if (jsonProperty!=null) {
+    			String value = jsonProperty.value();
+    			
+    			if (allowedFieldnames.contains(value)) {
+    				if (buffer.length()>0) buffer.append(",");
+        			buffer.append(value);
+    			}
+    			
+    		}
+    	}
+    	
+    	String fieldsWithJsonProperty = buffer.toString();
+    	
+    	// Load fields if no fields have been entered
+    	if (fieldlist==null || fieldlist.length()==0) {
+    		fieldlist = fieldsWithJsonProperty;
+    	}
+    	
+    	String url = apiUrl + name + "?access_token=" + access_token + "&fields=" + fieldlist;
     	System.out.println("url: " + url);
     	
-		FacebookUser user = restTemplate.getForObject(url, FacebookUser.class);
-				
-		return user;
+    	try {
+			FacebookUser user = restTemplate.getForObject(url, FacebookUser.class);
+					
+			return user;
+    	} catch (HttpClientErrorException e) {
+    		System.out.println("statusText: "+ e.getStatusText());
+    		System.out.println("message: "+ e.getMessage());
+    		throw new Exception(e.getMessage());
+    	}
 		 
 	} 
 	
