4dbdab8377594bedcaa7afdabbb3cf382fcd4afa
Johannes Fiala <johannes.fiala@fwd.at>
2015-11-06 22:15:06 +0100

    Finalize Facebook sample

---
+++ user-rest-service-2.2.2-facebook-client-java-codegen/src/test/java/at/fwd/swagger/spring/demo/user/util/AssembleFieldsUtil.java
@@ -0,0 +1,60 @@
+package at.fwd.swagger.spring.demo.user.util;
+
+import io.swagger.client.model.FacebookUser;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AssembleFieldsUtil {
+
+	
+	public static String assembleFieldsForFacebookUser() {
+		List<String> allowedFieldnames = new ArrayList<String>();
+    	allowedFieldnames.add("access_token");
+    	allowedFieldnames.add("id");
+    	allowedFieldnames.add("name");
+    	allowedFieldnames.add("about");
+    	allowedFieldnames.add("mission");
+    	allowedFieldnames.add("description");
+    	allowedFieldnames.add("app_id");
+    	allowedFieldnames.add("attire");
+    	
+    	List<String> fieldsWithHigherPermission = new ArrayList<String>();
+    	fieldsWithHigherPermission.add("promotion_eligible");
+    	fieldsWithHigherPermission.add("owner_business");
+    	fieldsWithHigherPermission.add("ad_campaign");
+    	
+    	List<String> nestedDatatypes = new ArrayList<String>();
+    	nestedDatatypes.add("location");
+    	nestedDatatypes.add("context");
+    	nestedDatatypes.add("cover");
+    	nestedDatatypes.add("app_links");
+    	nestedDatatypes.add("parking");
+    	nestedDatatypes.add("voip_info");
+    	nestedDatatypes.add("engagement");
+    	
+    	StringBuffer buffer = new StringBuffer();
+    	
+    	Method[] methods = FacebookUser.class.getMethods();
+    	for (Method method : methods) {
+    		JsonProperty jsonProperty =  method.getAnnotation(JsonProperty.class);
+    		
+    		if (jsonProperty!=null) {
+    			String value = jsonProperty.value();
+    			
+    			if (!fieldsWithHigherPermission.contains(value) && !nestedDatatypes.contains(value)) {
+    				if (buffer.length()>0) buffer.append(",");
+        			buffer.append(value);
+    			}
+    			
+    		}
+    	}
+    	
+    	String fieldsWithJsonProperty = buffer.toString();
+    	
+    	return fieldsWithJsonProperty;
+	}
+}
---
+++ user-rest-service-2.2.2-simple/src/main/java/at/fwd/swagger/spring/demo/user/model/State.java
@@ -0,0 +1,11 @@
+package at.fwd.swagger.spring.demo.user.model;
+
+/**
+ * Demo for enum State
+ * 
+ * @author johannes.fiala@fwd.at
+ *
+ */
+public enum State {
+	ACTIVE, DISABLED;
+}
