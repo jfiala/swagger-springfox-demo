c8ad34783d030307de0d3668dc963c23afbff49e
Johannes Fiala <johannes.fiala@fwd.at>
2015-11-12 16:45:34 +0100

    Add customized Java language for demo

---
+++ swagger-codegen-2.1.3-facebook/src/main/java/io/swagger/codegen/languages/JavaFacebookClientCodegen.java
@@ -0,0 +1,26 @@
+package io.swagger.codegen.languages;
+
+import io.swagger.codegen.CodegenConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JavaFacebookClientCodegen extends JavaClientCodegen implements CodegenConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JavaFacebookClientCodegen.class);
+
+    public JavaFacebookClientCodegen() {
+        super();
+        templateDir = "Java_Facebook";
+       
+    }
+    
+    @Override
+    public String getName() {
+        return "JavaFacebook";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Generates a Java client library.";
+    }
+}
