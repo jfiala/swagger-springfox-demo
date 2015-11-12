9f97fd1697d339c4a170ac38639b2c47853c0fa0
Johannes Fiala <johannes.fiala@fwd.at>
2015-11-12 11:01:22 +0100

    hide BasicErrorController

--- user-rest-service-2.2.2-facebook/src/main/java/at/fwd/swagger/spring/demo/user/Application.java
+++ user-rest-service-2.2.2-facebook/src/main/java/at/fwd/swagger/spring/demo/user/Application.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import at.fwd.swagger.spring.demo.user.controller.connectors.FacebookController;
 //replaces: @ComponentScan @EnableAutoConfiguration @Configuration
+
+import com.google.common.base.Predicates;
 
 /**
  * Spring Boot Root Configuration
@@ -33,12 +36,14 @@ public class Application {
     @Bean
     public Docket swaggerSpringMvcPlugin() {
       return new Docket(DocumentationType.SWAGGER_2)
-              .apiInfo(apiInfo())
+      	.select() 
+      	.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+      	.build()
+      	.apiInfo(apiInfo())
               .directModelSubstitute(java.util.Calendar.class, Long.class);
       
     }
     
-
     private ApiInfo apiInfo() {
 	      ApiInfo apiInfo = new ApiInfo(
 	              "My Apps API Title", "Demo Swagger Spring 2 API Description", "1.0",
